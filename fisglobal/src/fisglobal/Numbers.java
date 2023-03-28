e41bd0dc41b0747aa25c1279d60ed9f7deaeced6
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2023-03-28 17:01:18 +0530

    Add files via upload

---
+++ fisglobal/src/fisglobal/Numbers.java
@@ -0,0 +1,22 @@
+package fisglobal;
+
+import java.util.Scanner;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=160;
+		double d = 20.3446;
+		System.out.println("Inside the main method");
+		System.out.println("I am in java class");
+		System.out.println("we are freshers to fis global");
+		System.out.println(i); //----> 10
+		System.out.println(d);
+		System.out.println(10+20);
+		System.out.println("Hello "+" hai");
+
+		
+
+	}
+}
---
+++ fisglobal/src/fisglobal/Test.java
@@ -0,0 +1,20 @@
+package fisglobal;
+import java.util.Scanner;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter value");
+		int x = sc.nextInt();
+		System.out.println("The value entered is :"+x);
+		System.out.println("Enter a double value");
+		double d = sc.nextDouble();
+		System.out.println("Enter a string");
+		String str = sc.next();
+		System.out.println("The doube value is : "+d);
+		System.out.println("The strng is : "+str);
+
+	}
+}
